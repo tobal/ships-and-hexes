e712511144e06556f13acc36e2202c9ee3c596f3
Tobal <tobal17@gmail.com>
2014-12-15 17:58:17 +0100

    Main: mouse pointer now visible

--- dev/src/Main.cpp
+++ dev/src/Main.cpp
@@ -150,11 +150,87 @@ void drawMap() {
 	drawMapBackground();
 }
 
+// TODO
+orxVIEWPORT *viewport;
+orxOBJECT *mouseCursor;
+
+void orxFASTCALL Update(const orxCLOCK_INFO *_pstClockInfo, void *_pstContext)
+{
+//  orxVECTOR vScale, vPosition;
+//
+//  /* Is rotate left input active ? */
+//  if(orxInput_IsActive("RotateLeft"))
+//  {
+//    /* Rotates Parent object CCW */
+//    orxObject_SetRotation(pstParentObject, orxObject_GetRotation(pstParentObject) - orxMATH_KF_PI * _pstClockInfo->fDT);
+//  }
+//  /* Is rotate right input active? */
+//  if(orxInput_IsActive("RotateRight"))
+//  {
+//    /* Rotates Parent object CW */
+//    orxObject_SetRotation(pstParentObject, orxObject_GetRotation(pstParentObject) + orxMATH_KF_PI * _pstClockInfo->fDT);
+//  }
+//
+//  /* Is scale up input active? */
+//  if(orxInput_IsActive("ScaleUp"))
+//  {
+//    /* Scales up the Parent object */
+//    orxObject_SetScale(pstParentObject, orxVector_Mulf(&vScale, orxObject_GetScale(pstParentObject, &vScale), orx2F(1.02f)));
+//  }
+//  /* Is scale down input active? */
+//  if(orxInput_IsActive("ScaleDown"))
+//  {
+//    /* Scales down the Parent object */
+//    orxObject_SetScale(pstParentObject, orxVector_Mulf(&vScale, orxObject_GetScale(pstParentObject, &vScale), orx2F(0.98f)));
+//  }
+
+	orxVECTOR vPosition;
+	/* Is mouse in viewport? */
+	if(orxRender_GetWorldPosition(orxMouse_GetPosition(&vPosition), viewport, &vPosition))
+	{
+		orxVECTOR vParentPosition;
+		orxObject_GetWorldPosition(mouseCursor, &vParentPosition);
+		vPosition.fZ = vParentPosition.fZ;
+		orxObject_SetPosition(mouseCursor, &vPosition);
+	}
+}
+
 orxSTATUS orxFASTCALL Init()
 {
 	orxConfig_Load("cfg/mainConf.ini");
-	orxViewport_CreateFromConfig("Viewport");
+	viewport = orxViewport_CreateFromConfig("Viewport");
+
+	mouseCursor = orxObject_CreateFromConfig("CursorObj");
 	drawMap();
+
+
+	orxINPUT_TYPE   eType;
+	orxENUM         eID;
+	const orxSTRING zInputUp;
+	const orxSTRING zInputDown;
+	const orxSTRING zInputLeft;
+	const orxSTRING zInputRight;
+
+	orxInput_GetBinding("Up", 0, &eType, &eID);
+	zInputUp = orxInput_GetBindingName(eType, eID);
+
+	orxInput_GetBinding("Down", 0, &eType, &eID);
+	zInputDown = orxInput_GetBindingName(eType, eID);
+
+	orxInput_GetBinding("Left", 0, &eType, &eID);
+	zInputLeft = orxInput_GetBindingName(eType, eID);
+
+	orxInput_GetBinding("Right", 0, &eType, &eID);
+	zInputRight = orxInput_GetBindingName(eType, eID);
+
+
+	orxCLOCK *pstMainClock;
+	pstMainClock = orxClock_FindFirst(orx2F(-1.0f), orxCLOCK_TYPE_CORE);
+	orxClock_Register(pstMainClock, Update, orxNULL, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL);
+
+
+
+
 	return orxSTATUS_SUCCESS;
 }
 
@@ -166,7 +242,6 @@ orxSTATUS orxFASTCALL Run()
 	{
 		eResult = orxSTATUS_FAILURE;
 	}
-
 
 	return eResult;
 }
